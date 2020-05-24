b91b6c593b85faa840ee2348764c0eec0f50d3e6
zhongtaisong <zhongtaisong@sina.com>
2020-05-24 15:58:17 +0800

    更新接口前缀urll

--- src/config/index.js
+++ src/config/index.js
@@ -7,9 +7,7 @@ import settlementPageState from '@pages/SettlementPage/state';
 // tabBar - mobx数据
 import footerCopyrightState from '@com/footerCopyright/state';
 // url前缀
-const PUBLIC_URL = 'http://192.168.2.105:8000/api/';
-// const PUBLIC_URL = 'http://172.16.92.41:8000/api/';
-// const PUBLIC_URL = 'http://192.168.3.9:8000/api/';
+const PUBLIC_URL = 'http://127.0.0.1:8000/api/';
 // 加密key
 const PWD_KEY = 'fjreqjklnvmsn9u9084931jj雨色轻风意柔情怜花殇12612132212';
 // 路由黑名单
